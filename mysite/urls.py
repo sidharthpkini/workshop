eb0b9efa2e99b29c98822ebd4755d12a00458e89
sidharthpkini <1414sidharth@gmail.com>
2016-06-06 15:59:49 +0530

    second

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     url(r'register/', include(reg_urls)),
     url(r'^user/login/$',anonymous_required(auth_views.login),{'template_name': 'register/in.html'},name='in'),
     url(r'^user/logout/$',auth_views.logout,{'template_name': 'logout.html'},name='logout'),
+    url(r'^chocolate/add/', AddChocolateView.as_view(),name="addchocolate")
 ]
--- registration/views.py
+++ registration/views.py
@@ -5,10 +5,13 @@ from django.core.urlresolvers import reverse_lazy
 from braces.views import LoginRequiredMixin, AnonymousRequiredMixin
 from django.views.generic.edit import FormView, UpdateView
 from registration.forms import *
+from django.views.generic import ListView
 
-class HomePageView( TemplateView):
+class HomePageView(ListView):
 	template_name = "index.html"
 
+	def get_queryset(self):
+		return Chocolate.objects.all()
 
 
 class UserRegistrationView(AnonymousRequiredMixin, FormView):
@@ -18,5 +21,15 @@ class UserRegistrationView(AnonymousRequiredMixin, FormView):
 	success_url = '/register/user/success/'
 
 	def form_valid(self, form):
-	    form.save()
-	    return FormView.form_valid(self, form)
+		form.save()
+		return FormView.form_valid(self, form)
+
+
+class AddChocolateView(FormView):
+	template_name = "addchocolate.html"
+	form_class = ChocolateAddForm
+	success_url = '/register/chocolate/success/'
+
+	def form_valid(self, form):
+		form.save()
+		return FormView.form_valid(self, form)
